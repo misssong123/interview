7daa8fb12792e5f4ab5feaf9995a0d0e4b5e519c
misssong123 <2442600094@qq.com>
2020-05-11 23:08:46 +0800

    小知识总结

---
+++ src/com/meng/LittleKnowledge.java
@@ -0,0 +1,11 @@
+package com.meng;
+
+/**
+ * 记录常见的小知识
+ */
+public class LittleKnowledge {
+    public static void main(String[] args) {
+        //1.查看机器核数
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
